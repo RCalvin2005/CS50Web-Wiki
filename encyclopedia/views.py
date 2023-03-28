60839fd0e0b52f78964e401209f3ef09e2b8dc76
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-03-28 12:01:21 +0700

    Added search functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 from markdown2 import markdown
 
 
 def index(request):
+    """ Displays list of all entries """
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
@@ -28,3 +31,37 @@ def entry(request, title):
         "title": title,
         "content": markdown(content)
     })
+
+
+def search(request):
+    """ Allow user to search entry in sidebar """
+
+    if request.method == "POST":
+
+        # https://stackoverflow.com/questions/4706255/how-to-get-value-from-form-field-in-django-framework
+        q = request.POST.get("q")
+        entries = util.list_entries()
+        matches = []
+
+        # Goes through each entry and check if it matches the query
+        for entry in entries:
+
+            # Redirect if full match
+            if entry.lower() == q.lower():
+
+                # https://docs.djangoproject.com/en/4.1/ref/urlresolvers/#reverse
+                return HttpResponseRedirect(reverse("entry", args=[entry]))
+            
+            # Record partial matches
+            if q.lower() in entry.lower():
+                matches.append(entry)
+
+        # Renders page with partial match results
+        return render(request, "encyclopedia/results.html", {
+            "q": q,
+            "matches": matches
+        })
+
+    else:
+        # Redirect to index if /search URL is visited
+        return HttpResponseRedirect(reverse("index"))
